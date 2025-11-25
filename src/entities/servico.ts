d5966cdf6b922ae2786adbfff70637736d253e1a
Micael Contin Pucci <micaelpucci06@gamil.com>
2025-11-25 19:37:44 -0300

    Ajuste entities

--- src/entities/servico.ts
+++ src/entities/servico.ts
@@ -1,10 +1,5 @@
-export class Servico {
-  constructor(
-    public tipo: string,
-    public data: Date,
-    public prazo: number,
-    public garantia: number,
-    public clienteId: number,
-    public id?: number
-  ) {}
-}
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export default prisma.cliente
