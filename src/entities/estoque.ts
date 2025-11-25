1b4ab9cba4d011991612547f46b0af9947ab3b92
Micael Contin Pucci <micaelpucci06@gamil.com>
2025-11-25 19:58:30 -0300

    Ajuste entities

--- src/entities/estoque.ts
+++ src/entities/estoque.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client"
+import {PrismaClient} from "@prisma/client"
 
 const prisma = new PrismaClient()
 
-export default prisma.cliente
+export default prisma.estoque
