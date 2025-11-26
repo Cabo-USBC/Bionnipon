4067413591e6264bd05dbe78f3e8db726da18629
Kenzosh <gabrielkenzooshima@gmail.com>
2025-11-25 21:14:58 -0300

    Ajustes Routes

--- src/routes/cliente.routes.ts
+++ src/routes/cliente.routes.ts
@@ -2,14 +2,12 @@
 import { Router } from "express"
 import * as ClienteController from "../controllers/cliente.controllers.js"
 import { validateDto } from "../middlewares/validate.dto.js"
-import { CreateUserDto } from "../dtos/create-user.dto.js"
-import { UpdateUserDto } from "../dtos/update-user.dtos.js"
 
 const router = Router()
-router.post("/client", validateDto(CreateUserDto), ClienteController.createCliente)
+router.post("/client", ClienteController.createCliente)
 router.get("/client", ClienteController.getAllClientes)
 router.get("/client/:id", ClienteController.getClienteById)
-router.put("/client/:id", validateDto(UpdateUserDto), ClienteController.updateUser)
+router.put("/client/:id", ClienteController.updateUser)
 router.delete("/client/:id", ClienteController.deleteUser)
 
 export default router
