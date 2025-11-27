a6edc6ba2025ba1b4d4c19b7f3bb1233275b8939
Thiago Fernando <thiagofernando024@gmail.com>
2025-11-27 09:56:46 -0300

    ajuste dos clientes para rodar o server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import "dotenv/config"
 import express from "express"
+import clienteRoutes from "./routes/cliente.routes.js"
 
 const app = express()
 app.use(express.json())
+app.use(clienteRoutes)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
