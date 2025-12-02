4358e27583d316fbb764e6e35bfd101aa054708b
Micael Contin Pucci <micaelpucci06@gamil.com>
2025-12-01 22:36:23 -0300

    dnv

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import "dotenv/config"
 import express from "express"
 import clientRoutes from "./routes/cliente.routes.js"
+import ServicoRoutes from "./routes/servico.routes.js" 
 
 
 const app = express()
 app.use(express.json())
 app.use(clientRoutes)
+app.use(ServicoRoutes)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
